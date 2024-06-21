ea0504beaf7557b522b05a6df067a42cdbac616a
N acicor <sa456x12@gmail.com>
2024-06-22 03:08:35 +0800

    fix : UI fix

--- app/src/main/java/com/example/github_users/ui/theme/Theme.kt
+++ app/src/main/java/com/example/github_users/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.github_users.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
