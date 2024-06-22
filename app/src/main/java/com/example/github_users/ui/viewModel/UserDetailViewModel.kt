150ecb83a03086356bc02b3be495021ece5d0a3c
N acicor <sa456x12@gmail.com>
2024-06-22 17:03:08 +0800

    feat : error handling http error

--- app/src/main/java/com/example/github_users/ui/viewModel/UserDetailViewModel.kt
+++ app/src/main/java/com/example/github_users/ui/viewModel/UserDetailViewModel.kt
@@ -2,20 +2,34 @@ package com.example.github_users.ui.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.github_users.domain.model.UserDetail
 import com.example.github_users.domain.usecase.GetUserDetailDataUseCase
+import com.example.github_users.utils.UserDetailState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class UserDetailViewModel(private val getUserDetailDataUseCase: GetUserDetailDataUseCase):ViewModel() {
-    private val _userDetail = MutableStateFlow<UserDetail?>(null)
+class UserDetailViewModel(private val getUserDetailDataUseCase: GetUserDetailDataUseCase) :
+    ViewModel() {
+    private val _userDetail = MutableStateFlow<UserDetailState>(UserDetailState.Loading)
     val userDetail = _userDetail.asStateFlow()
 
     fun fetchUserDetail(userLogin: String) {
         viewModelScope.launch {
-            val detail = getUserDetailDataUseCase.execute(userLogin)
-            _userDetail.value = detail
+            try {
+                _userDetail.value = UserDetailState.Loading
+                val result = getUserDetailDataUseCase.execute(userLogin)
+                result.fold(
+                    onSuccess = {
+                        _userDetail.value = UserDetailState.Success(it)
+                    },
+                    onFailure = {
+                        _userDetail.value =
+                            UserDetailState.Error(it.message ?: "An unexpected error occurred")
+                    }
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
